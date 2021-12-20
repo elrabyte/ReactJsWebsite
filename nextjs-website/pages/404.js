7076bf3fc284a2f0f9ba2253ddef1990ad095834
Rabyte <saint.barco@gmail.com>
2021-12-20 16:26:20 +0100

    404 page

--- nextjs-website/pages/404.js
+++ nextjs-website/pages/404.js
@@ -1,6 +1,17 @@
 import Link from 'next/link'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 const NotFound = () => {
+    
+    const router = useRouter();
+
+    useEffect(() => {
+        setTimeout(()=> {
+            router.push("/");
+        }, 5000)
+    },[])
+
     return ( 
         <main>
             <section class="text-center container">
@@ -8,7 +19,7 @@ const NotFound = () => {
                     <div class="mx-auto">
                         <h1 class="fw-light">You fucked up</h1>
                         <p class="lead text-muted">this page doesn't exist</p>
-                        <p>go back <Link href="/">home</Link></p>
+                        <p>you will be redirected back to the <Link href="/">homepage</Link>.</p>
                     </div>
                 </div>
             </section>
--- nextjs-website/pages/_app.js
+++ nextjs-website/pages/_app.js
@@ -4,6 +4,7 @@ import "../styles/fontawesome/all.min.js"
 
 import { useEffect } from "react";
 import Layout from '../comps/layout';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
@@ -12,6 +13,9 @@ function MyApp({ Component, pageProps }) {
   
   return (
     <Layout>
+      <Head>
+        <title>Test</title>
+      </Head>
       <Component {...pageProps} />
     </Layout>
   )
